296c568855a9593aa4a2ef4c004a8f7e26c796ff
Ryunosuke Nagoka <69971830+nagaoka1166@users.noreply.github.com>
2020-12-31 00:05:49 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Rect from './Rect';
+import './App.css';
+
+let data = {title:'Title',
+           message:'this is sample message.'};
+
+const SampleContext = React.createContext(data);
+
+
+class App extends Component {
+ 
+ render(){
+    return (
+        <div>
+        <h1>Context</h1>
+        <Title />
+        <Message />
+        </div>
+        );
+    
+    }
+}
+class Title extends Component {
+    
+   render() {
+       return (
+       <div>
+             <h2>{this.context.title}</h2>
+       </div>
+       );
+   }
+}
+
+//class Message extends Component {
+//    static contextType = SampleContext;
+//   
+//      render(){
+//          return (
+//          <div>
+//          <p>{this.context.title}</p>
+//          </div>
+//              );
+//      }
+//}
+
+class Message extends Component {
+    static contextType = SampleContext;
+  render(){
+  return(
+  　　<div>
+       <p>{this.context.message}</p>
+     </div>
+      );
+    }
+}
+    
+
+ 
+
+export default App;
+
+
+                        
